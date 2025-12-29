67752f44902e0cad83b44a6f984defc04a8e73b3
willbritto <ailson.h@outlook.com>
2025-12-29 15:35:05 -0300

    Criado o modelos das tabelas que irei usar no projeto

---
+++ OrderWebAPI/Models/CategoryModel.cs
@@ -0,0 +1,14 @@
+﻿namespace OrderWebAPI.Models;
+
+public class CategoryModel
+{
+
+    public int CategoryId { get; set; }
+
+    public string? Service_Type { get; set; }
+
+
+    public int OrderId { get; set; }
+
+    public OrderModel OrderModel { get; set; }
+}
---
+++ OrderWebAPI/Models/OrderModel.cs
@@ -0,0 +1,16 @@
+﻿namespace OrderWebAPI.Models;
+
+public class OrderModel
+{
+    public int OrderId { get; set; }
+    public int NumOrder { get; set; }
+
+    public string? Descricao { get; set; }
+
+    public decimal Preco { get; set; }
+
+
+    public CategoryModel CategoryModel { get; set; }
+
+
+}
