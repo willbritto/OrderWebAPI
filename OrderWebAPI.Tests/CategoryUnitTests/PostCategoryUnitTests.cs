254b46c117fa9972475d25dcee6c60e2c20f89f7
willbritto <ailson.h@outlook.com>
2026-01-04 18:26:46 -0300

    Implementação do xUnity e algumas configurações de segurança

---
+++ OrderWebAPI.Tests/CategoryUnitTests/PostCategoryUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderWebAPI.Data;
+using OrderWebAPI.Models;
+using OrderWebAPI.Services;
+
+namespace OrderWebAPI.Tests.CategoryUnitTests
+{
+    public class PostCategoryUnitTests
+    {
+        [Fact]
+
+        public async Task CreateCategory_ShouldAddCategory() 
+        {
+
+
+            //Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            using var context = new ApplicationDbContext(options);
+
+            var service = new CategoryService(context);
+            var category = new CategoryModel { CategoryId = 2, Service_Type = "Upgrade" };
+
+            //Act
+            var result = await service.CreateCategory(category);
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal("Upgrade", result.Service_Type);
+            Assert.Single(context.categoryModels);
+        }
+
+    }
+}
