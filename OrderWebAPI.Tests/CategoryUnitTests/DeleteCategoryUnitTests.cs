3b3502f6f304d73f5046e7568caab8d39e70defc
willbritto <ailson.h@outlook.com>
2026-01-04 21:31:34 -0300

    Adicionado as classes para realização do teste xUnit

---
+++ OrderWebAPI.Tests/CategoryUnitTests/DeleteCategoryUnitTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderWebAPI.Tests.CategoryUnitTests
+{
+    public class DeleteCategoryUnitTests
+    {
+    }
+}
